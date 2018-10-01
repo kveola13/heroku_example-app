1949255521f02978585c265f8d64f73bb785cef7
Ola Kveli <olakveli@yahoo.com>
2018-10-01 12:00:49 +0200

    added name

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello Worldz!";
+      return "Hello Ola!";
     }
 
     public static void main(String[] args) {
